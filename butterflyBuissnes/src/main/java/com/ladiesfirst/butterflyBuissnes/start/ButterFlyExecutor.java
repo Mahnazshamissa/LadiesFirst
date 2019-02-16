e50c4849c652d711e0dfc0dd126b5be5d5377a2b
BartvaiErika <erika.bartvai@gmail.com>
2019-02-17 00:59:51 +0100

    rows

--- butterflyBuissnes/src/main/java/com/ladiesfirst/butterflyBuissnes/start/ButterFlyExecutor.java
+++ butterflyBuissnes/src/main/java/com/ladiesfirst/butterflyBuissnes/start/ButterFlyExecutor.java
@@ -4,6 +4,11 @@ import com.ladiesfirst.butterflyBuissnes.fileReader.FileLoader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Service
 @RequiredArgsConstructor
 public class ButterFlyExecutor {
@@ -11,8 +16,18 @@ public class ButterFlyExecutor {
     private final FileLoader fileLoader;
 
     public void read() {
-        fileLoader.asStream("buterfly-business.txt")
-                .forEach(s -> System.out.println(s));
+        List<String> allRows = fileLoader.asList("buterfly-business.txt");
+        List<List<String>> splitedRows = IntStream.range(0, allRows.size())
+                .boxed()
+                .filter(i -> i % 3 == 0)
+                .map(i -> extractThreeRowsFrom(allRows, i))
+                .collect(Collectors.toList());
     }
+
+    private List<String> extractThreeRowsFrom(List<String> allRows, Integer i) {
+        return Arrays.asList(allRows.get(i), allRows.get(i + 1), allRows.get(i + 2));
+    }
+
+
 
 }
